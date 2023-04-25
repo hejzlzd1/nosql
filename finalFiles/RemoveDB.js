95beda87228330e13b9f37be4ebeeea8c4549272
hejzlzd1 <hejzlzd1@uhk.cz>
2023-04-25 12:47:26 +0200

    data import - fix this

--- finalFiles/RemoveDB.js
+++ finalFiles/RemoveDB.js
@@ -1,2 +1,15 @@
-use ('FriendWorker');
-db.dropDatabase();
+const { exec } = require('child_process');
+//TODO fix me
+// Command to execute
+var connectToMongoSH = 'docker exec -i mongodbPrimary mongosh';
+
+// Execute command in the terminal
+exec(connectToMongoSH, (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error executing command: ${err.message}`);
+        return;
+    }
+    console.log("Successfull connection to db");
+
+
+});
