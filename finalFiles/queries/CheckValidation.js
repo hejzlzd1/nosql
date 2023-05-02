2990504524ae160d58c87ad37a718b75ceffb089
hejzlzd1 <hejzlzd1@uhk.cz>
2023-05-02 12:35:16 +0200

    Validation examples added

--- finalFiles/queries/CheckValidation.js
+++ finalFiles/queries/CheckValidation.js
@@ -2,22 +2,40 @@ use('FriendWorker');
 try{
 db.users.insertOne(
     {
-        "firstName": "",
-        "lastName": "",
-        "email": "rreolfo0@@sciencedaily.com",
+        "firstName": "arnold",
+        "lastName": "netrefil validaci",
+        "email": "arny01moczavinacu@@sciencedaily.com",
         "dateOfBirth": {
-            "$date": "2007-07-31T13:06:00Z"
+            "$date": "No-one expects string on date field!"
         },
+        "password": "testovaci heslo, ktere absolutne nesplnuje validaci :) Tato validace me naprosto odmitne a neprijme dokument",
         "address": {
-            "city": "Cagnes-sur-Mer",
-            "postal": "629 392",
-            "street": "311 American Alley",
-            "country": "France"
+            "city": "Cagnes-sur-Mer (long text validation error)",
+            "postal": "629 392 (long text validation error)",
+            "street": "311 American Alley (long text validation error)",
+            "country": "France is great country (long text validation error)"
         }
     }
 )}
 catch (e){
-    console.log("User validator error");
+    console.log("User validator error (value validation)");
+    printjson(e.errInfo)
+    console.log("\n");
+}
+
+try{
+    db.users.insertOne(
+        {
+            "address": {
+                "city": "Cagnes-sur-Mer",
+                "postal": "629 392",
+                "street": "311 American Alley",
+                "country": "France"
+            }
+        }
+    )}
+catch (e){
+    console.log("User validator error (missing required)");
     printjson(e.errInfo)
     console.log("\n");
 }
@@ -43,21 +61,58 @@ db.posts.insertOne(
         ]
     })
 }catch (e){
-    console.log("Post validator error");
+    console.log("Post validator error (value validation)");
         printjson(e.errInfo)
+    console.log("\n");
+}
+
+try{
+    db.posts.insertOne(
+        {
+            "comments": [
+                {
+                    "id": "comment_497"
+                },
+                {
+                    "id": "comment_340"
+                }
+            ],
+            "likes": [
+                {
+                    "id": "like_428"
+                }
+            ]
+        })
+}catch (e){
+    console.log("Post validator error (missing required)");
+    printjson(e.errInfo)
+    console.log("\n");
+}
+
+
+try{
+    db.messages.insertOne(
+        {
+            "author": 10,
+            "receiver": true,
+            "seen": "I saw this one!"
+        }
+    )
+} catch (e){
+    console.log("Message validator error (value validation)");
+    printjson(e.errInfo)
     console.log("\n");
 }
 
 try{
     db.messages.insertOne(
         {
-            "author": "user_570",
-            "receiver": "user_8",
+            "author": "user_1",
             "seen": true
         }
     )
 } catch (e){
-    console.log("Message validator error");
+    console.log("Message validator error (missing required)");
     printjson(e.errInfo)
     console.log("\n");
 }
@@ -66,11 +121,23 @@ try{
     db.likes.insertOne(
     {
         "_id": "like_8",
-        "author": "user_788"
+        "author": 10
     }
 )
 }catch (e){
-    console.log("Like validator error");
+    console.log("Like validator error (value validation)");
+    printjson(e.errInfo)
+    console.log("\n");
+}
+
+try{
+    db.likes.insertOne(
+        {
+            "_id": "like_8",
+        }
+    )
+}catch (e){
+    console.log("Like validator error (missing required)");
     printjson(e.errInfo)
     console.log("\n");
 }
@@ -80,7 +147,7 @@ db.comments.insertOne(
     {
         "_id": "comment_5",
         "author": "user_276",
-        "text": "",
+        "text": true,
         "post": "post_19",
         "replies": [
             {
@@ -91,7 +158,20 @@ db.comments.insertOne(
     }
 )
 }catch (e){
-    console.log("Comment validator error");
+    console.log("Comment validator error (value validation)");
+    printjson(e.errInfo)
+    console.log("\n");
+}
+
+try{
+    db.comments.insertOne(
+        {
+            "_id": "comment_5",
+            "text": "Wow you are missing lot of values! :)"
+        }
+    )
+}catch (e){
+    console.log("Comment validator error (missing required)");
     printjson(e.errInfo)
     console.log("\n");
 }
